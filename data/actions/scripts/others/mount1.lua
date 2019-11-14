8f85d5423d5963943a1fbf2515d6185aae66860f
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-11-13 23:44:38 -0300

    ajuste script que da montaria

--- data/actions/scripts/others/mount1.lua
+++ data/actions/scripts/others/mount1.lua
@@ -4,12 +4,10 @@ local effect = 30 -- efeito ao clicar no baú
 local storage = 34530 
 
 if(player:getStorageValue(cid, 34530) > 0) then
-	player:sendTextMessage(MESSAGE_INFO_DESCR, string.format('Você já recebeu sua mount.')
 return TRUE
 end
 ﻿
 player:addMount(437) -- Aqui é o ID da montaria, você pode encontrar em DATA/XML/mounts.xml.
-player:sendTextMessage(MESSAGE_INFO_DESCR, string.format('Você já recebeu sua mount.')
 toPosition:sendMagicEffect(CONST_ME_SOUND_RED)
 
 return TRUE
