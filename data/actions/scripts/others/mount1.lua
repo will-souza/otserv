4b79834c56c4627d1d69a7ede0e6ddd2dc95b408
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-11-15 00:48:59 -0300

    bau que da montaria

--- data/actions/scripts/others/mount1.lua
+++ data/actions/scripts/others/mount1.lua
@@ -3,7 +3,6 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if not player:hasMount(438) then
 		player:addMount(438)
 		player:say(mount.message, TALKTYPE_MONSTER_SAY)
-		item:remove(1)
 	else
 		player:sendTextMessage(19, "You already have this mount")
 	end
