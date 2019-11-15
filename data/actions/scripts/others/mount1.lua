69c3a60effffc5d67f33de0e9618115d2fb75d0b
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-11-15 00:54:52 -0300

    bau que da montaria

--- data/actions/scripts/others/mount1.lua
+++ data/actions/scripts/others/mount1.lua
@@ -1,5 +1,10 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	
+
+	local mount = 438
+
+	if not mount then
+		return true
+	end
 	if not player:hasMount(438) then
 		player:addMount(438)
 		player:say(mount.message, TALKTYPE_MONSTER_SAY)
