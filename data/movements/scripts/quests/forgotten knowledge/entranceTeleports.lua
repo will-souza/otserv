941f31cd9b81199e48cb78d82d365332d4926ad5
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-11-16 22:53:06 -0300

    teste liberar halls of hope e yalahar quest

--- data/movements/scripts/quests/forgotten knowledge/entranceTeleports.lua
+++ data/movements/scripts/quests/forgotten knowledge/entranceTeleports.lua
@@ -26,23 +26,34 @@ function onStepIn(creature, item, position, fromPosition)
 		return
 	end
 	if item.itemid == 11796 then
-		if player:getStorageValue(teleport.storage) >= 1 then
-		position:sendMagicEffect(teleport.effect)
-		player:teleportTo(teleport.newPos)
-		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-		return true
+		-- if player:getStorageValue(Storage.ForgottenKnowledge.AccessLast) < 1 then
+			if player:getStorageValue(Storage.ForgottenKnowledge.LadyTenebrisKilled) >= 1 and player:getStorageValue(Storage.ForgottenKnowledge.LloydKilled) >= 1 and
+				player:getStorageValue(Storage.ForgottenKnowledge.ThornKnightKilled) >= 1 and player:getStorageValue(Storage.ForgottenKnowledge.DragonkingKilled) >= 1 and
+				player:getStorageValue(Storage.ForgottenKnowledge.HorrorKilled) >= 1 and player:getStorageValue(Storage.ForgottenKnowledge.TimeGuardianKilled) >= 1 then
+
+				player:setStorageValue(Storage.ForgottenKnowledge.AccessLast, 1)
+			end
+		end
 	end
-	if player:getStorageValue(teleport.storage) >= 1 then
+	if player:getStorageValue(teleport.storage) >= 0 then
 		position:sendMagicEffect(teleport.effect)
 		player:teleportTo(teleport.newPos)
 		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		return true
 	else
-		if player:getStorageValue(teleport.storage) >= 1 then
-		position:sendMagicEffect(teleport.effect)
-		player:teleportTo(teleport.newPos)
+		if item.itemid == 11796 then
+			player:teleportTo(fromPosition)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You don\'t have the permission to use this portal.")
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			return true
+		end
+		local pos = position
+		pos.y = pos.y + 2
+		player:teleportTo(pos)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You don\'t have the permission to use this portal.")
 		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-		return true
+		pos.y = pos.y - 2
+		pos:sendMagicEffect(CONST_ME_TELEPORT)
 	end
 	return true
 end
