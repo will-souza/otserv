ef06d65f49a166311e4fa04c63b77fcceacaf7bb
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-11-17 01:54:37 -0300

    teste modal doll, yaman liberado

--- data/npc/scripts/Yaman.lua
+++ data/npc/scripts/Yaman.lua
@@ -82,12 +82,6 @@ end
 
 local function onTradeRequest(cid)
 	local player = Player(cid)
-	
-	if player:getStorageValue(Storage.DjinnWar.EfreetFaction.Mission03) ~= 3 then
-		npcHandler:say('I\'m sorry, but you don\'t have Malor\'s permission to trade with me.', cid)
-		return false
-	end
-
 	return true
 end
 
