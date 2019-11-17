516769fb70709fda5460b040dc7f23cd35b5922c
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-11-17 03:29:47 -0300

    teste nova voc, teste velocidade e forca atk

--- data/creaturescripts/scripts/onadvance_reward.lua
+++ data/creaturescripts/scripts/onadvance_reward.lua
@@ -5,14 +5,18 @@ local table =
     -- [level] = type = "addon", id = {ID_ADDON_FEMALE, ID_ADDON_MALE}, msg = "MENSAGEM"},
     -- [level] = type = "mount", id = {ID_MOUNT, 0}, msg = "MENSAGEM"},
 
-    [20] = {type = "item", id = {2160, 2}, msg = "Voce ganhou 2 crystal coins por alcancar o level 20!"},
-    [30] = {type = "bank", id = {20000, 0}, msg = "Foi depositado em seu bank 20000 gold coints!"},
-    [40] = {type = "addon", id = {136, 128}, msg = "Voce ganhou o addon citizen full por alcancar o level 40!"},
-    [50] = {type = "item", id = {2160, 2}, msg = "Voce ganhou 2 crystal coins por alcancar o level 20!"},
-    [60] = {type = "item", id = {2160, 2}, msg = "Voce ganhou 2 crystal coins por alcancar o level 20!"},
-    [70] = {type = "item", id = {2160, 2}, msg = "Voce ganhou 2 crystal coins por alcancar o level 20!"},
-    [80] = {type = "item", id = {2160, 2}, msg = "Voce ganhou 2 crystal coins por alcancar o level 20!"},
-    [90] = {type = "mount", id = {370, 0}, msg = "Voce ganhou a montaria x!"},
+    [20] = {type = "item", id = {2160, 5}, msg = "Voce ganhou 5 crystal coins por alcancar o level 20!"},
+    [100] = {type = "bank", id = {150000, 0}, msg = "Foi depositado em seu bank 150000 gold coints!"},
+    [100] = {type = "item", id = {2195, 2}, msg = "Voce ganhou uma boots of haste por alcancar o level 100!"},
+    [200] = {type = "addon", id = {136, 128}, msg = "Voce ganhou o addon citizen full por alcancar o level 200!"},
+    [200] = {type = "bank", id = {300000, 0}, msg = "Foi depositado em seu bank 300000 gold coints!"},
+    [300] = {type = "bank", id = {200000, 0}, msg = "Foi depositado em seu bank 200000 gold coints!"},
+    [300] = {type = "item", id = {20620, 2}, msg = "Voce ganhou 1 Zaoan chess box por alcancar o level 300!"},
+    [400] = {type = "item", id = {18511, 2}, msg = "Voce ganhou 1 Music box por alcancar o level 40!"},
+    [400] = {type = "bank", id = {200000, 0}, msg = "Foi depositado em seu bank 200000 gold coints!"},
+    [500] = {type = "item", id = {18511, 2}, msg = "Voce ganhou 1 Music box por alcancar o level 500!"},
+    [500] = {type = "bank", id = {200000, 0}, msg = "Foi depositado em seu bank 200000 gold coints!"},
+    [600] = {type = "mount", id = {1209, 0}, msg = "Voce ganhou a montaria Cerberus Champion!"},
 }
 
 local storage = 15000
