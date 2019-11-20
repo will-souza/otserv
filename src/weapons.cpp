a53b200ea1d741911c14e3dbac12014675a12fd1
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-11-20 12:40:55 -0300

    teste aumento damage

--- src/weapons.cpp
+++ src/weapons.cpp
@@ -151,7 +151,7 @@ bool Weapons::registerLuaEvent(Weapon* event)
 int32_t Weapons::getMaxMeleeDamage(int32_t attackSkill, int32_t attackValue)
 {
 	// editado era metade
-	return static_cast<int32_t>(std::ceil((attackSkill * (attackValue * 0.5)) + (attackValue * 0.5)));
+	return static_cast<int32_t>(std::ceil((attackSkill * (attackValue * 1)) + (attackValue * 1)));
 }
 
 //players
