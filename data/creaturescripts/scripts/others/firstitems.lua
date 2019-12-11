ddd1c109d75b878cae42a587a62c028bc4244a4f
Cruz <andre.cruz.doutoresdaweb@gmail.com>
2019-12-10 23:22:24 -0300

    itens iniciais

--- data/creaturescripts/scripts/others/firstitems.lua
+++ data/creaturescripts/scripts/others/firstitems.lua
@@ -7,21 +7,21 @@ local config = {
 	},
 	[1] = {
 		--equipment spellbook, wand of vortex, magician's robe, mage hat, studded legs, leather boots, scarf
-		items = {{2175, 1}, {2190, 1}, {8871, 1}, {10016, 1}, {2478, 1}, {11303, 1}, {2661, 1}},
+		items = {{2175, 1}, {2190, 1}, {8871, 1}, {2323, 1}, {2478, 1}, {11303, 1}, {2661, 1}},
 		--container platinum coin, rope, shovel, mana potion
 		container = {{2152, 20}, {7620, 25}, {18559, 1}, {2120, 1}}
 	},
 	[2] = {
 		--equipment spellbook, snakebite rod, magician's robe, mage hat, studded legs, leather boots scarf
-		items = {{2175, 1}, {2182, 1}, {8871, 1}, {10016, 1}, {2478, 1}, {11303, 1}, {2661, 1}},
+		items = {{2175, 1}, {2182, 1}, {8871, 1}, {2323, 1}, {2478, 1}, {11303, 1}, {2661, 1}},
 		--container platinum coin, rope, shovel, mana potion
 		container = {{2152, 20}, {7620, 25}, {18559, 1}, {2120, 1}}
 	},
 	[3] = {
 		--equipment dwarven shield, 5 spear, ranger's cloak, ranger legs scarf, legion helmet
-		items = {{2525, 1}, {2664, 1}, {2647, 1}, {11303, 1}, {2455, 1}, {2661, 1}, {36460, 1}},
+		items = {{2525, 1}, {2664, 1}, {2660, 1}, {2647, 1}, {11303, 1}, {2455, 1}, {2661, 1}, {36460, 1}},
 		--container platinum coin, rope, shovel, health potion, mana potion
-		container = {{2518, 1}, {2410, 1}, {2456, 1}, {2789, 25}, {2543, 1}, {2554, 1}}
+		container = {{2518, 1}, {2410, 1}, {2544, 1}, {2456, 1}, {2789, 25}, {2543, 1}, {2554, 1}}
 	},
 	[4] = {
 		--Scarf, Crown Helmet, Crown Armor, Crown Legs, Zaoan Shoes, Spike Sword, Bonelord Shield, Moon Backpack
